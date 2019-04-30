c424d87dc5289ee3c40924aa66f3055daf786d63
aUcid <aUcidme@users.noreply.github.com>
2019-05-01 00:20:16 +0800

    Initial Commit

---
+++ AiBeautify/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AiBeautify
+//
+//  Created by aUcid on 2019/5/1.
+//  Copyright © 2019 Dalian University Of Technology. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
